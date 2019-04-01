a4bd700622a99f7b7387f162dfbf09768edcc957
Yannick <comte.yannick@gmail.com>
2019-04-01 07:53:28 +0200

    New component for screenshots, including 360

--- Assets/Scripts/TES/Components/Editor/ScreenshotCapturerEditor.cs
+++ Assets/Scripts/TES/Components/Editor/ScreenshotCapturerEditor.cs
@@ -3,17 +3,23 @@ using UnityEngine;
 
 namespace TESUnity.Components.Utilities
 {
-    [CustomEditor(typeof(ScreenshotCapturer))]
+    [CustomEditor(typeof(ScreenshotMaker))]
     public class ScreenshotCapturerEditor : Editor
     {
         public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
 
-            var script = (ScreenshotCapturer)target;
+            var script = (ScreenshotMaker)target;
 
             if (GUILayout.Button("Capture Screenshot"))
                 script.CaptureScreenshot();
+
+            if (GUILayout.Button("Capture Screenshot 360"))
+                script.Capture360Screenshot(false);
+
+            if (GUILayout.Button("Capture Screenshot 360 3D"))
+                script.Capture360Screenshot(true);
         }
     }
 }
